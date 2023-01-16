ca208443a355acb23599aa520760ade34c8acdc3
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-16 23:00:13 +0800

    feat: define -file flag to specify file name
    added a black run function that the in case statemet -file that
    determines if the flag is called to

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 const (
 	header = `<!DOCTYPE html>
 <html>
@@ -16,5 +22,31 @@ const (
 )
 
 func main() {
+	// Parse flags
+	filename := flag.String("file", "", "Markdown file preview")
 
+	flag.Parse()
+
+	// Put flag in a container that we can check later if the flag is used
+	usedFlags := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) {
+		usedFlags[f.Name] = true
+	})
+
+	switch {
+	case usedFlags["file"]:
+		if *filename == "" {
+			flag.Usage()
+			os.Exit(1)
+		}
+	}
+
+	if err := run(*filename); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(fileName string) error {
+	return nil
 }
