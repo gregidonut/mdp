896a1ea20167479ee367e8d36f1d2518b12a4b86
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-16 22:44:59 +0800

    feat: define constants header and footer
    the black friday package generates the content based on the input
    Markdow, but doesn't include HTML headers and footers required to view
    it in a web browser

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+const (
+	header = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<title>Markdown Preview Tool</title>
+	</head>
+	<body>
+`
+	footer = `
+	</body>
+</html>
+`
+)
+
+func main() {
+
+}
