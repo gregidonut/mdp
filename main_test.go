042efb802bc920d9f7ae132072d83973cf05f257
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-17 02:54:56 +0800

    test: update main_test.go to use run() with mocked bytes.Buffer{} output
    completing the reason the io.Writer interface was used as a parmater by
    updating the TestRun() case with a generic case mock

--- main_test.go
+++ main_test.go
@@ -3,12 +3,12 @@ package main
 import (
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 )
 
 const (
 	inputFile  = "./testdata/test1.md"
-	resultFile = "test1.md.html"
 	goldenFile = "./testdata/test1.md.html"
 )
 
@@ -33,9 +33,13 @@ func Test_parseContent(t *testing.T) {
 }
 
 func TestRun(t *testing.T) {
-	if err := run(inputFile); err != nil {
+	mockStdOut := bytes.Buffer{}
+
+	if err := run(inputFile, &mockStdOut); err != nil {
 		t.Fatal(err)
 	}
+
+	resultFile := strings.TrimSpace(mockStdOut.String())
 
 	want, err := os.ReadFile(resultFile)
 	if err != nil {
