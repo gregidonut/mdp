a097903926f1a574b3a0cf05e43809f83d3d4a16
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-18 10:57:20 +0800

    test: add initial implementation for integration test
    implemented some boiler plate to build the cli and then run test cases
    
    the first test case is just a sort of initial use cases which have
    already been tested with TestRun() just as a sanity check. the main
    point of this is to prepare for a test where setting up the env var will
    make a difference

--- main_test.go
+++ main_test.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -87,6 +92,88 @@ func TestRun(t *testing.T) {
 			//time.Sleep(tt.delay)
 
 			os.Remove(resultFile)
+		})
+	}
+}
+
+var binName = "mdp"
+
+// TestMain executes the go build tool to create the binary of the cli
+// then runs it with arguments to check if outputs are expected
+// then cleans up the files after the function is completed.
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+	err := build.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %q: %s", binName, err)
+	}
+
+	fmt.Println("Running tests...")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+
+	os.Exit(result)
+}
+
+func TestMDPCLI(t *testing.T) {
+	tests := []struct {
+		name           string
+		goldenFileName string
+		flags          []string
+		specifyEnvVar  bool
+	}{
+		{
+			name:           "WithoutEnvVar -s -file " + inputFile,
+			flags:          []string{"-s", "-file", inputFile},
+			goldenFileName: goldenFileDefault,
+		},
+		{
+			name:           "WithoutEnvVar -s -file " + inputFile + " -t " + templateFile1,
+			flags:          []string{"-s", "-file", inputFile, "-t", templateFile1},
+			goldenFileName: goldenFileWithTemplateFile1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := os.Getwd()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			cmdPath := filepath.Join(dir, binName)
+			cmd := exec.Command(cmdPath, tt.flags...)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			t.Logf("output: \n%#q\n", string(out))
+
+			outputFileContents, err := os.ReadFile(strings.TrimSpace(string(out)))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			goldenFileContents, err := os.ReadFile(tt.goldenFileName)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			got := string(outputFileContents)
+			want := string(goldenFileContents)
+
+			if got != want {
+				t.Errorf("got != want, got: \n%s\n golden: %q \n%s\n", got, tt.goldenFileName, want)
+			}
 		})
 	}
 }
